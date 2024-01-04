0f9d2806896f7d1c6fc0b1e9d491f3fa6ea928dc
kamal <kamal@onelap.in>
2024-01-04 16:30:16 +0530

    first commit

---
+++ src/main/java/com/gongo/djangouser/DjangoUserApplication.java
@@ -0,0 +1,13 @@
+package com.gongo.djangouser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DjangoUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DjangoUserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gongo/djangouser/DjangoUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gongo.djangouser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DjangoUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
